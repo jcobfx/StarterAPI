01822758a079736b3366fd1916519bef8ad7ef21
jcobfx <118930070+jcobfx@users.noreply.github.com>
2025-05-12 01:44:54 +0200

    fix Swagger profile annotation; change from 'dev & prod' to 'dev | prod' for correct profile activation

--- src/main/java/pl/com/foks/starterapi/config/SwaggerConfig.java
+++ src/main/java/pl/com/foks/starterapi/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev & prod")
+@Profile("dev | prod")
 public class SwaggerConfig {
 
     @Bean
